237cd6a9a3e2dc3df89f9e126538c8f0fbbb6a26
wangsong <xingyuchicheng@outlook.com>
2024-10-04 07:53:34 +0800

    feat: 全局异常添加

---
+++ src/main/java/org/ifs/yapi/common/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package org.ifs.yapi.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public YResult exceptionHandler(Exception e) {
+        log.error(e.getMessage(), e);
+        return YResult.error(500, e.getMessage());
+    }
+}
